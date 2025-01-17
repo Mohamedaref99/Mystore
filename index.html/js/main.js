253c54959c97400e4063f55ce2644e1478b28c32
Mohamedaref99 <116679496+Mohamedaref99@users.noreply.github.com>
2025-01-18 01:53:38 +0400

    Update main.js

--- index.html/js/main.js
+++ index.html/js/main.js
@@ -1,14 +1,6 @@
-// Function to add products to the cart
 function addToCart(productName, productPrice) {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const existingProductIndex = cart.findIndex(item => item.name === productName);
-    
-    if (existingProductIndex !== -1) {
-        cart[existingProductIndex].quantity += 1;  // Increase quantity if product already exists
-    } else {
-        cart.push({ name: productName, price: productPrice, quantity: 1 });
-    }
-
+    cart.push({ name: productName, price: productPrice });
     localStorage.setItem('cart', JSON.stringify(cart));
     alert(`${productName} has been added to your cart!`);
 }
